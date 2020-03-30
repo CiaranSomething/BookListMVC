e08e2b54a46ca4ed956aef8df5b3c23785c9d840
Ciaran Corrigan <ciaran_corrigan@hotmail.com>
2020-03-30 15:06:59 +0100

    Connect to SQLDB in Azure

--- BookListMVC/Startup.cs
+++ BookListMVC/Startup.cs
@@ -25,9 +25,21 @@ namespace BookListMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-                );
+//            services.AddDbContext<ApplicationDbContext>(
+//               options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+//              );
+
+            //Use SQL database if in Azure, otherwise use local SQL db
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<ApplicationDbContext>(options => 
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            else
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Automatically perform database migration
+            services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
+
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
